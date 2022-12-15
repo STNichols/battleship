1608ef11cfb0b37c8922914bb8dc195264ab17b7
Sean Nichols <sean.nichols22@gmail.com>
2022-12-15 14:20:31 -0500

    Adding skeleton classes

---
+++ src/missile.py
@@ -0,0 +1,9 @@
+"""
+Missile Class for simulating a missile
+"""
+
+
+class Missile:
+
+    def __init__(self):
+        pass
---
+++ src/ship.py
@@ -0,0 +1,9 @@
+"""
+Ship Class for simulating a ship
+"""
+
+
+class Ship:
+
+    def __init__(self):
+        pass
